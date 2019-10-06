d2c1e13bb5a8e44a0e42c3e5aa57c8459c5cf2fa
Emrullah Ayın <ayinemrullah@gmail.com>
2019-10-06 15:23:58 +0300

    sass config

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
-const {parallel} = require("gulp");
+const { src, dest, watch, series } = require("gulp"),
+  connect = require("gulp-connect"),
+  sass = require("gulp-sass"),
+  concat = require("gulp-concat");
 
-function javascript(cb) {
-    cb();
+function connectServer(cb) {
+  connect.server({
+    root: "./",
+    port: 3000,
+    livereload: true
+  });
 }
 
-function css(cb){
-    cb();
+function scssConcat() {
+  return src("./src/sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat("main.min.css"))
+    .pipe(dest("./build"));
 }
 
-
-exports.default = series(javascript,css);
+exports.default = series(scssConcat, connectServer);
